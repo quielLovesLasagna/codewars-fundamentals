8a2385b63940661452bc580159efa8cd62396bbc
Exequiel Arco <137391972+quielLovesLasagna@users.noreply.github.com>
2023-11-23 09:35:34 +0800

    Add files via upload

---
+++ bool-to-string/script.js
@@ -0,0 +1,7 @@
+/*
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false. 
+Link: https://www.codewars.com/kata/53369039d7ab3ac506000467/train/javascript
+*/
+
+// Solution:
+const boolToWord = (bool) => (bool ? "Yes" : "No");
