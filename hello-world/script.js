30eb3714865a2e19c6bf9699c0334461b6278b0e
Exequiel Arco <137391972+quielLovesLasagna@users.noreply.github.com>
2023-11-23 11:10:00 +0800

    Add files via upload

---
+++ hello-world/script.js
@@ -0,0 +1,8 @@
+/*
+Make a simple function called greet that returns the most-famous "hello world!".
+
+Link: https://www.codewars.com/kata/523b4ff7adca849afe000035/train/javascript
+*/
+
+// Solution:
+const greet = () => "hello world!";
---
+++ summation/script.js
@@ -0,0 +1,14 @@
+/*
+Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+Link: https://www.codewars.com/kata/55d24f55d7dd296eb9000030/train/javascript
+*/
+
+// Solution:
+const summation = (num) => {
+	let sum = 0;
+	for (let i = 1; i <= num; i++) {
+		sum += i;
+	}
+	return sum;
+};
