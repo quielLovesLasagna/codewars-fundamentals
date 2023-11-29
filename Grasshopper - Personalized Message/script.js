1bf64e08e45fe61ba4a360123ec63650c7868445
Exequiel Arco <137391972+quielLovesLasagna@users.noreply.github.com>
2023-11-29 22:24:55 +0800

    Add files via upload

---
+++ Grasshopper - Personalized Message/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/*
+Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+Use conditionals to return the proper message:
+
+case	return
+name equals owner	'Hello boss'
+otherwise	'Hello guest'
+
+Link: https://www.codewars.com/kata/5772da22b89313a4d50012f7/train/javascript
+*/
+
+// Solution:
+const greet = (name, owner) => (name === owner ? "Hello boss" : "Hello guest");
