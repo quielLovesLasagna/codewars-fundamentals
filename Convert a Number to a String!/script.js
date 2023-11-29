17ff1cafbfad6432174b14e4be66242f893da750
Exequiel Arco <137391972+quielLovesLasagna@users.noreply.github.com>
2023-11-29 15:07:52 +0800

    Add files via upload

---
+++ Convert a Number to a String!/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/*
+We need a function that can transform a number (integer) into a string.
+
+Link: https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/javascript
+*/
+
+// Solution:
+
+const numberToString = (num) => "" + num;
