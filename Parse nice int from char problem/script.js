aefc2f108ad91d685b2453c103c6c1c54c7ebbff
Exequiel Arco <137391972+quielLovesLasagna@users.noreply.github.com>
2023-12-04 21:13:56 +0800

    Add files via upload

---
+++ Parse nice int from char problem/script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/*
+You ask a small girl,"How old are you?" She always says, "x years old", where x is a random number between 0 and 9.
+
+Write a program that returns the girl's age (0-9) as an integer.
+
+Assume the test input string is always a valid string. For example, the test input may be "1 year old" or "5 years old". The first character in the string is always a number.
+
+Link: https://www.codewars.com/kata/557cd6882bfa3c8a9f0000c1/train/javascript
+*/
+
+// Solution:
+function test(inputString) {
+	const age = inputString.split(" ")[0];
+	console.log(parseInt(age));
+}
+
+const getAge = (inputString) => parseInt(inputString.split(" ")[0]);
+
+// Other solutions:
+const getAge2 = (inputString) => parseInt(inputString[0]);
+const getAge3 = (inputString) => parseInt(inputString);
+
+// Test
+console.log(getAge("23"));
