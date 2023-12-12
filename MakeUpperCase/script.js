e9bc79d5fc7ab40b9f44b975ecdcd1c05682dc62
Quiel <137391972+quielLovesLasagna@users.noreply.github.com>
2023-12-12 16:32:25 +0800

    Add files via upload

---
+++ MakeUpperCase/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/*
+Write a function which converts the input string to uppercase.
+
+Link: https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/train/javascript
+*/
+
+// Solution:
+const makeUpperCase = (str) => str.toUpperCase();
