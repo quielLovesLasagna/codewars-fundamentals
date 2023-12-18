33a137fe866275265edcff13e468cc29c95badc9
Quiel <137391972+quielLovesLasagna@users.noreply.github.com>
2023-12-18 21:31:03 +0800

    Add files via upload

---
+++ Function 2 - squaring an argument/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/*
+Now you have to write a function that takes an argument and returns the square of it.
+
+Link: https://www.codewars.com/kata/523b623152af8a30c6000027/train/javascript
+*/
+
+// Solution:
+const square = (num) => num ** 2;
