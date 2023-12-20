1ae9b4d093bb280870b1610a92523ab22e3c2a44
Quiel <137391972+quielLovesLasagna@users.noreply.github.com>
2023-12-20 10:22:01 +0800

    Add files via upload

---
+++ Beginner - Lost Without a Map/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/*
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+
+Link: https://www.codewars.com/kata/57f781872e3d8ca2a000007e/train/javascript
+*/
+
+// Solution:
+const maps = (x) => x.map((num) => num * 2);
